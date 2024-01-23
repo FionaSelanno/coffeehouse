85e9f532dc5157093b9d0cae1415d9012998ce44
Fiona Selanno <fcs_fiona@hotmail.com>
2024-01-23 17:24:52 +0100

    add carrousel

---
+++ index.js
@@ -0,0 +1,44 @@
+let prev = document.querySelector("#prev");
+let next = document.querySelector("#next");
+let dotsArr= document.querySelectorAll(".slider__dot");
+let list = document.querySelector(".slider__list");
+let itemsArr = document.querySelectorAll(".slider__item");
+let index = 0;
+let len = itemsArr.length - 1;
+
+prev.addEventListener('click', goToSlide)
+next.addEventListener('click', goToSlide)
+
+function goToSlide(event){
+    var clickedElement = event.target;
+    let idElem = clickedElement.id;
+    console.log(idElem);
+    if (idElem == "next")
+    {
+        if (index >= len)
+            index = 0;
+        else
+            index++;
+        list.style.left = -(index * 480)+ 'px';
+        updateNav(index);
+
+    }
+    else if (idElem == "prev")
+    {
+        if (index <= 0)
+            index = 2;
+        else
+            index--;
+        console.log("inde: " + index + "len: " + len);
+        list.style.left = -(960 - (len - index) * 480) + 'px';
+        updateNav(index);
+    }
+}
+
+
+function updateNav(index){
+    let active = document.querySelector('.slider__dot--active');
+    active.classList.remove('slider__dot--active');
+    console.log(dotsArr[index]);
+    dotsArr[index].classList.add('slider__dot--active');
+}
